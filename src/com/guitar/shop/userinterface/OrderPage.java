ce7e455f31d40fe4359fad17de1d7754a8d6d6af
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2020-10-16 13:02:46 +0200

    OrderPage Code

--- src/com/guitar/shop/userinterface/OrderPage.java
+++ src/com/guitar/shop/userinterface/OrderPage.java
@@ -2,16 +2,20 @@ package com.guitar.shop.userinterface;
 
 import com.guitar.shop.datalayer.Database;
 import com.guitar.shop.model.Article;
+import com.guitar.shop.model.ArticleType;
 import com.guitar.shop.model.Manager;
 import com.guitar.shop.model.SalesRepresentative;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -22,16 +26,18 @@ public class OrderPage {
     private Database db;
     private ObservableList<SalesRepresentative> salesrepresentative;
     private ObservableList<Manager> managers;
+    private ObservableList<Article> articles;
     public OrderPage(){
 
+        stage = new Stage();
+        stage.setTitle("GuitarshopFX - Create an Order");
         // initialise data
         db = new Database();
         salesrepresentative = FXCollections.observableArrayList(db.getSalespersons());
         managers = FXCollections.observableArrayList(db.getManagers());
+        //articles = FXCollections.checkedObservableList();
 
-        HBox layout = new HBox();
-        layout.setPadding(new Insets(20));
-        layout.setSpacing(20);
+
 
         Label makeOrderLabel = new Label();
         makeOrderLabel.setText("Create Order # 1000002");
@@ -52,13 +58,58 @@ public class OrderPage {
 
         TableColumn quantityCol = new TableColumn("Quantity");
         quantityCol.setMinWidth(100);
-        quantityCol.
+        quantityCol.setCellValueFactory(new PropertyValueFactory<Article,Integer>("quantity"));
 
+        TableColumn bandCol = new TableColumn("Band");
+        bandCol.setMinWidth(100);
+        bandCol.setCellValueFactory(new PropertyValueFactory<Article, String>("band"));
 
+        TableColumn modelCol = new TableColumn("Model");
+        modelCol.setMinWidth(100);
+        modelCol.setCellValueFactory(new PropertyValueFactory<Article, String>("model"));
 
+        TableColumn acousticCol = new TableColumn("Acoustic");
+        acousticCol.setMinWidth(100);
+        acousticCol.setCellValueFactory(new PropertyValueFactory<Article, Boolean>("Acoustic"));
 
+        TableColumn typeCol = new TableColumn("Type");
+        typeCol.setMinWidth(100);
+        typeCol.setCellValueFactory(new PropertyValueFactory<Article, ArticleType>("type"));
 
+        TableColumn priceCol = new TableColumn("Price");
+        priceCol.setMinWidth(100);
+        priceCol.setCellValueFactory(new PropertyValueFactory<Article, Double>("price"));
 
+        articleTableview.getColumns().addAll(quantityCol,bandCol,modelCol,acousticCol,typeCol,priceCol);
+        articleTableview.setItems(articles);
+
+        Button addButton = new Button();
+        addButton.setText("Add");
+
+        Button deleteButton = new Button();
+        deleteButton.setText("Remove");
+
+        Button confirmButton = new Button();
+        confirmButton.setText("Confirm");
+
+        Button resetButton = new Button();
+        resetButton.setText("Reset");
+
+        VBox layout = new VBox();
+        //layout.setPadding(new Insets(10));
+        layout.setSpacing(20);
+
+        HBox form = new HBox();
+        //form.setPadding(new Insets(10));
+        form.setSpacing(20);
+
+        form.getChildren().addAll(addButton,deleteButton,confirmButton,resetButton);
+
+        Scene scene = new Scene(layout);
+        stage.setScene(scene);
+        stage.show();
+
+        
     }
 
 }
